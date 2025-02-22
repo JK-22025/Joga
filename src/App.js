40f21339d2a9f6f5c00adb08c4857572a4c310f6
JK-22025 <kazanj7@gmail.com>
2025-02-22 18:38:05 -0500

    joga

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-// App.js
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Signup from './Signup';
 import Login from './Login';
 import Home from './Home';
-<<<<<<< HEAD
-=======
 import Kids from './Kids';
 import Kits from './Kits';
->>>>>>> e28297f (jogaretro)
 
 const App = () => {
   const [user, setUser] = useState(null);
@@ -22,7 +18,7 @@ const App = () => {
   };
 
   const handleLogout = () => {
-    setUser(null);  // Clear user state on logout
+    setUser(null);
   };
 
   return (
@@ -32,7 +28,8 @@ const App = () => {
         <Link to="/Kits">Kits</Link>
         <Link to="/Kids">Kids</Link>
         <Link to="/Contact">Contact</Link>
-<<<<<<< HEAD
+        <Link to="/signup">Signup</Link>
+        <Link to="/login">Login</Link>
       </nav>
       <Routes>
         <Route
@@ -40,24 +37,11 @@ const App = () => {
           element={user ? <Home user={user} onLogout={handleLogout} /> : <Login onLogin={handleLogin} />}
         />
         <Route path="/signup" element={<Signup onSignup={handleSignup} />} />
-=======
-        <Link to ="/signup">Signup</Link>
-        <Link to ="/login">Login</Link>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home  />} />
-        <Route
-          path="/login"
-          element={user ? <Home user={user} onLogout={handleLogout} /> : <Login onLogin={handleLogin} />}
-        />
-        <Route path="/signup" element={<Signup onSignup={handleSignup} />} />
+        <Route path="/login" element={<Login onLogin={handleLogin} />} />
         <Route path="/kids" element={<Kids />} />
         <Route path="/kits" element={<Kits />} />
-
->>>>>>> e28297f (jogaretro)
       </Routes>
     </Router>
-    
   );
 };
 
--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,8 @@
-// Home.js
 import React, { useState } from 'react';
 import './Home.css';
-import Navbar from './Navbar.js';
-<<<<<<< HEAD
-
-const Home = ({ user }) => {
-=======
 import myImage from './assets/1.jpg';
 
-const Home = () => {
->>>>>>> e28297f (jogaretro)
+const Home = ({ user }) => {
   const [searchTerm, setSearchTerm] = useState('');
 
   const handleSearch = (e) => {
@@ -17,37 +10,25 @@ const Home = () => {
   };
 
   return (
-<<<<<<< HEAD
-    <div className="home-container">
-      <div className="logo">Joga Bonito Retro</div>
-      <div className="welcome-message">Welcome, {user.email}!</div>
-=======
     <div>
-    <div className="home-container">
-      <div className="logo">Joga Bonito Retro</div>
-      
-
->>>>>>> e28297f (jogaretro)
-      <div className="search-bar">
-        <input
-          type="text"
-          placeholder="Search..."
-          value={searchTerm}
-          onChange={handleSearch}
-        />
-<<<<<<< HEAD
-      </div>
-=======
-    </div>
+      <div className="home-container">
+        <div className="logo">Joga Bonito Retro</div>
+        <div className="welcome-message">Welcome, {user.email}!</div>
+        <div className="search-bar">
+          <input
+            type="text"
+            placeholder="Search..."
+            value={searchTerm}
+            onChange={handleSearch}
+          />
+        </div>
         <br />
-       
         <div>
           <img style={{ width: '100px', height: '100px' }} src={myImage} alt="My Image" />
         </div>
       </div>
-    
->>>>>>> e28297f (jogaretro)
     </div>
   );
 };
+
 export default Home;
