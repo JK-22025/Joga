f2e111d958a31752da1cd75acc6afa3b95662cbf
JK-22025 <kazanj7@gmail.com>
2025-03-16 01:55:46 -0400

    jogaretro

--- src/Kits.js
+++ src/Kits.js
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useState } from "react";  
 import "./Kits.css";
 
-   
 
+const Kits = ({ user }) => {
+    const [searchTerm, setSearchTerm] = useState('');
 
-const Kits = () => {
+    const handleSearch = (e) => {
+        setSearchTerm(e.target.value);
+    };
+
     return (
-        <div>
+        <div className="kits-container">
             <h1>Joga Bonito Retro</h1>
-            
-            
+
+            <div className="search-bar">
+                <input
+                    type="text"
+                    placeholder="Search..."
+                    value={searchTerm}
+                    onChange={handleSearch}
+                />
+            </div>
         </div>
     );
 };
