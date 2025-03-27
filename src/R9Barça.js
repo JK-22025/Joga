b9c3ad1cb78dbf9d24f8f2fab0b3fa1a08e62d3a
JK-22025 <kazanj7@gmail.com>
2025-03-27 12:43:05 -0400

    jogaretro

--- src/R9Barça.js
+++ src/R9Barça.js
@@ -4,37 +4,37 @@ import './R9_Barça.css';
 
 const R9_Barcelona = () => {    
     return (
-        <div className="kit-image">
-            <img src={RonaldoNaz} alt="RonaldoNaz" />
-            <div className="kit-details">
-                <h2>Ronaldo Nazario 96/97 Barcelona away kit </h2>
-                <p>Price: $150</p>
-                <div className="size-selection">
-                    <p>Select Size:</p>
-                    <div className="size-selection">
-                        <button>S</button>
-                        <button>M</button>
-                        <button>L</button>
-                        <button>XL</button>
-                    </div>
-                    
-                </div>
-                <div className="quantity-selection">
-                    <p>Quantity:</p>
-                    <input type="number" min="1" max="20" defaultValue="1" />
-                </div>
-                <p className="kit-description">
-                    Ronaldo Nazario 96/97 Barcelona away kit where he had an amazing season with Barcelona where he scored 47 goals in 51 games.
-                </p>
-                <div className="buttons">
-                    <button className="add-to-cart-button">Add to Cart</button>
-                    <button className="apple-pay">Apple Pay</button>
-                </div>
-            </div>
+        <div className="kit">
+            <table>
+                <tbody>
+                <tr>
+                    <td><img src={RonaldoNaz} alt="Ronaldo Nazario Kit" /></td>
+                    <td>
+                        <h2>Ronaldo Nazario 96/97 Barcelona away kit</h2>
+                        <p><strong>Price:</strong> $150</p>
+                        <p className="kit-description">
+                            Ronaldo Nazario 96/97 Barcelona away kit where he had an amazing season with Barcelona where he scored 47 goals in 51 games.
+                        </p>
+                        <div className="size-selection">
+                            <p><strong>Select Size:</strong></p>
+                            <button>S</button>
+                            <button>M</button>
+                            <button>L</button>
+                            <button>XL</button>
+                        </div>
+                        <div className="buttons">
+                            <button className="add-to-cart-button">Add to Cart</button>
+                            <button className="apple-pay">Apple Pay</button>
+                            <button className="google-pay">Google Pay</button>
+                        </div>
+                    </td>
+                </tr>
+
+                </tbody>
+                
+            </table>
         </div>
-        
-        
-    )
-}
+    );
+};
 
 export default R9_Barcelona;
